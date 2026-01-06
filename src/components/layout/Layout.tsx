bda838c690e45a2e6c43dafab2b80d910e43b1ad
Richard Kindler <223166946+RickCreator87@users.noreply.github.com>
2026-01-06 03:56:47 -0700

    Create Layout.tsx
    Signed-off-by: Richard Kindler <223166946+RickCreator87@users.noreply.github.com>

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Sidebar from './Sidebar'
+import TopBar from './TopBar'
+
+interface LayoutProps {
+  children: React.ReactNode
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Sidebar />
+      <div className="lg:pl-64">
+        <TopBar />
+        <main className="py-6 px-4 sm:px-6 lg:px-8">
+          <div className="max-w-7xl mx-auto">
+            {children}
+          </div>
+        </main>
+      </div>
+    </div>
+  )
+}
+
+export default Layout
